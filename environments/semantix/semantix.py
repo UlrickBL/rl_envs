85fae35b1d6b7aa65384f99d18cd1f32e6345c69
BLE <u.ble@orange.fr>
2025-09-03 14:31:46 +0000

    add dataset and Env, WIP for reward and check answer

--- environments/semantix/semantix.py
+++ environments/semantix/semantix.py
@@ -27,22 +27,48 @@ TURN_PROMPT = """Your last guess was: "{GUESS}"
 Similarity score with the target word: {SCORE}
 
 Here are the top 10 closest words to your guess:
-{WORD1}: {SIM1}
-{WORD2}: {SIM2}
-...
-{WORD10}: {SIM10}
+{TOP_WORDS}
 
 Based on this feedback, provide your next single-word guess in {LANGUAGE}.
 """
+
+def prepare_dataset(dataset_len,lang) :
+    data = []
+    for i in range(dataset_len) :
+        data.append(
+            {
+                "prompt": [{"role": "user", "content": GAME_PROMPT.format(LANGUAGE=lang)}],
+                "info": {
+                    "ground_truths": get_random_word(lang=lang),
+                    "turn_num": 0,
+                    "guesses": [],
+                    "similarities": [],
+                },
+            }
+        )
 
 def get_random_word(lang="en", n=5000):
     words = top_n_list(lang, n)
     return random.choice(words)
 
-def count_turns_reward_func(parser, completion, answer, **kwargs) -> float:
-    num_turns = len([x for x in completion if x["role"] == "assistant"])
-    is_correct = check_answer_reward_func(parser, completion, answer, **kwargs)
-    return is_correct / (num_turns + 1)
+def check_answer(parser, completion, answer, **kwargs) -> bool: # TODO
+    guess = parser.parse_answer(completion)
+    return guess == "[" + answer + "]"
+
+def create_weighted_rewards(): # TODO
+    """
+    Reward could be something like :
+    base_reward = 0 if format issue, similarity if not reached (so between 0 and 1) and 2 if proper word found
+    reward=base_reward * exp(-α*turn)
+    weight it with the max reward available
+    """
+    def weighted_reward(completion, state, **kwargs):
+        actual_turns = state["info"]["num_turns"]
+        best_similarity = max(state["info"]["similarities"])
+        return 
+
+    return weighted_reward
+
 
 def get_similarity(model,ground_truth,guess) :
     embeddings = model.encode([ground_truth, guess], convert_to_tensor=True)
@@ -60,40 +86,58 @@ class SemantixEnv(vf.MultiTurnEnv):
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.similarity_model = args.similarity_model
 
     def is_completed(self, messages: Messages, state: State, **kwargs) -> bool:
+        """
+        Multiturn is completed when 
+        - Number of turn is reached
+        - Correct word is sent
+        """
         assert isinstance(messages, list)
         assistant_count = len([m for m in messages if m["role"] == "assistant"])
         num_turns = state["info"]["num_turns"]
-        return assistant_count >= num_turns
+        
+        if assistant_count >= num_turns :
+            return True
+        else :
+            return check_answer(parser, completion, answer) # TODO
 
     def env_response(self, messages: Messages, state: State, **kwargs) -> Tuple[Messages, State]:
-        assert isinstance(messages, list)
-        assistant_count = len([m for m in messages if m["role"] == "assistant"])
-        num_turns = state["info"]["num_turns"]
-
-        if assistant_count < num_turns:
-            follow_ups = state["info"]["follow_ups"]
-            follow_up_idx = assistant_count - 1
-
-            if follow_up_idx < len(follow_ups):
-                return [{"role": "user", "content": follow_ups[follow_up_idx]}], state
-
-        return [{"role": "user", "content": "Continue"}], state
+        """
+        Env response is basically TURN_PROMPT completed with the correct values
+        """
+        assistant_msgs = [m["content"] for m in messages if m["role"] == "assistant"]
+        parser = vf.XMLParser(fields=["guess"], answer_field="guess")
+        guess = parser.parse_answer(assistant_msgs[state["info"]["turn_num"] - 1])
+        similarity = get_similarity(self.similarity_model,state["ground_truth"],guess)
+        
+        state["info"]["turn_num"] += 1
+        state["info"]["guesses"].append(guess)
+        state["info"]["similarities"].append(similarity)
+        
+        pairs = list(zip(state["guesses"], state["similarities"]))
+        top_pairs = sorted(pairs, key=lambda x: x[1], reverse=True)[:10]
+        top_words_str = "\n".join([f"{w}: {s:.2f}" for w, s in top_pairs])
+        
+        return [{"role": "user", "content": TURN_PROMPT.format(GUESS=guess,SCORE=similarity,TOP_WORDS=top_words_str,LANGUAGE=state["language"])}], state
         
 def load_environment(model_name = "paraphrase-multilingual-MiniLM-L12-v2",
-                     lang="en") -> vf.Environment:
+                     lang="en",
+                     max_turns=30,
+                     dataset_len=10000) -> vf.Environment:
     '''
     Loads environment for semantic
     '''
-    model = SentenceTransformer(model_name)
-    ground_truth = get_random_word(lang=lang)
+    similarity_model = SentenceTransformer(model_name)
+    
+    words_dataset = prepare_dataset(dataset_len,lang)
 
     system_prompt = NOTHINK_GUESS_SYSTEM_PROMPT
     
     parser = vf.XMLParser(fields=["guess"], answer_field="guess")
-    rubric = vf.Rubric(parser=parser)
-    rubric.add_reward_func(check_answer_reward_func)
-    rubric.add_reward_func(parser.get_format_reward_func(), weight=0.2)
+    rubric = vf.Rubric(parser=parser,funcs=[create_weighted_rewards()], weights=[1.0])
     
-    raise NotImplementedError("Implement your custom environment here.")
+    env_instance = SemantixEnv(dataset=words_dataset, rubric=rubric, max_turns=max_turns, system_prompt=system_prompt, similarity_model=similarity_model)
+    
+    return env_instance
